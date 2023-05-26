a0e1f8a436afb5589175a908daf659b114fde95c
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-26 19:58:46 +0700

    menampilkan data

--- Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
+++ Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
@@ -227,5 +227,21 @@ void DoubleLinkedList::searchData() {
 		}
 	}
 
+	int main() {
+		DoubleLinkedList obj;
+		while (true) {
+			try {
+				cout << "\nMenu" << endl;
+				cout << "1. Add a record to the list" << endl;
+				cout << "2. Delete a record from the list" << endl;
+				cout << "3. View all records in the ascending order of roll numbers" << endl;
+				cout << "4. View all records in the descending order of roll numbers" << endl;
+				cout << "5. Saerch for a record in the list" << endl;
+				cout << "6. Exit" << endl;
+				cout << "\nEnter your choice (1-6): ";
+				char ch;
+				cin >> ch;
+
+
 
 
