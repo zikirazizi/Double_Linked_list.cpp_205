12cdf5bc0a5f5c164e063c3285979bf653b35220
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-26 19:58:06 +0700

    membuat ruang linglist

--- Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
+++ Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
@@ -208,4 +208,24 @@ void DoubleLinkedList::searchData() {
 			cout << "Record with roll number " << rollNo << " deleted" << endl;
 	}
 
+	void DoubleLinkedList::searchData() {
+		if (listEmpty() == true) {
+			cout << "\nList is empty" << endl;
+
+		}
+		Node* prev, * curr;
+		prev = curr = NULL;
+		cout << "\nEnter the roll number of the student whose record you want to search: ";
+		int num;
+		cin >> num;
+		if (DoubleLinkedList::search(num, &prev, &curr) == false)
+			cout << "\nRecord not found" << endl;
+		else {
+			cout << "\nRecord found" << endl;
+			cout << "\nRoll number: " << curr->noMhs << endl;
+			cout << "\nName: " << curr->name << endl;
+		}
+	}
+
+
 
