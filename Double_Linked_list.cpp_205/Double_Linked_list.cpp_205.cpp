42090e21319ce923826303eac92232a2e304dd04
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-26 19:47:20 +0700

    membuat step

--- Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
+++ Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
@@ -62,3 +62,16 @@ void DoubleLinkedList::addNode() {
 		previous = current;			// 1.d
 		current = current->next;	// 1.e
 	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next;		// Step 4
+	newNode->prev = current;			// Step 5
+	if (current->next != NULL)
+		current->next->prev = newNode;	// Step 6
+	current->next = newNode;			// Step 7
+}
+
