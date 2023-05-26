7c083f4f06bcf0a8cfa94f89fefd8ff3c7cf9368
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-26 19:56:57 +0700

    membuat ruang

--- Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
+++ Double_Linked_list.cpp_205/Double_Linked_list.cpp_205.cpp
@@ -155,3 +155,57 @@ void DoubleLinkedList::hapus() {
 void DoubleLinkedList::searchData() {
 	if (listEmpty() == true) {
 		cout << "\nList is empty" << endl;
+
+	}
+
+	bool DoubleLinkedList::listEmpty() {
+		return (START == NULL);
+	}
+
+	void DoubleLinkedList::traverse() {
+		if (listEmpty())
+			cout << "\nList is empty" << endl;
+		else {
+			cout << "nRecords in ascending order of roll number are:" << endl;
+			Node* currentNode = START;
+			while (currentNode != NULL) {
+				cout << currentNode->noMhs << " " << currentNode->name << endl;
+				currentNode = currentNode->next;
+			}
+		}
+	}
+
+	void DoubleLinkedList::revtraverse() {
+		if (listEmpty()) {
+			cout << "\nList is empty" << endl;
+		}
+
+		else {
+			cout << "\nRecords in descending order of roll number are:" << endl;
+			Node* currentNode = START;
+			while (currentNode->next != NULL)
+				currentNode = currentNode->next;
+
+			while (currentNode != NULL) {
+				cout << currentNode->noMhs << " " << currentNode->name << endl;
+				currentNode = currentNode->prev;
+			}
+		}
+	}
+
+	void DoubleLinkedList::hapus() {
+		if (listEmpty()) {
+			cout << "\nList is empty" << endl;
+
+		}
+		cout << "\nEnter the roll number of the student whose record is to be deketed: ";
+		int rollNo;
+		cin >> rollNo;
+		cout << endl;
+		if (DoubleLinkedList::deleteNode(rollNo) == false)
+			cout << "Record not found" << endl;
+		else
+			cout << "Record with roll number " << rollNo << " deleted" << endl;
+	}
+
+
